36648d5b93635d880ed368448c02876f4bd2fd3d
Deise Kelley <daysekelley@live.com>
2018-02-12 01:58:20 -0200

    deixei mais bonito e com .php
    devaneios

---
+++ cadastro.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>  <!-- -->
+<html>
+
+<title>Cadastro</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap core CSS -->
+  <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="starter-template.css" rel="stylesheet">
+
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="javascript.js"></script>
+</head>
+
+
+  <body>
+
+
+
+    <div id="header">
+      <div id="logo"><img src="imagens/devaneioslogo.png" width=467px; /></div>
+       <ul id="navbar">
+             <li><a href="index.php">Home</a></li>
+             <li><a href="sobre.php">Sobre</a></li>
+             <li><a href="tirinhas.php">Tirinhas</a></li>
+         <li class="dropdown">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+       <ul id="login-dp" class="dropdown-menu">
+         <li>
+            <div class="row">
+               <div class="col-md-12">
+                  <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                     <div class="form-group">
+                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email" required>
+                     </div>
+                     <div class="form-group">
+                        <label class="sr-only" for="exampleInputPassword2">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Senha" required>
+                     </div>
+                     <div class="form-group">
+                          <button type="submit" class="btn btn-primary btn-block">Logar</button>
+                       </div>
+                    </form>
+                 </div>
+                 <div class="bottom text-center">
+                  Não tem cadastro? <a href="cadastro.php"><b>Cadastre-se</b></a>
+                 </div>
+              </div>
+           </li>
+         </ul>
+           </li>
+              <li><a href="lojavirtual.php">Loja Virtual</a></li>
+         </ul>
+       </div><!-- /.navbar-collapse -->
+
+
+<div class="container">
+ <div id="divCenter">
+    <form action="#" method="post">
+      <fieldset>
+          <fieldset class="grupo">
+            <div class="campo">
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" name="nome" style="width: 10em" value="">
+            </div>
+           </fieldset>
+            <fieldset class="grupo">
+              <div class="campo">
+                  <label for="snome">Rua</label>
+                  <input type="text" id="snome" name="snome" style="width: 10em" value="">
+              </div>
+              <div class="campo">
+                  <label for="snome">Número</label>
+                  <input type="text" id="snome" name="snome" style="width: 10em" value="">
+              </div>
+              <div class="campo">
+                  <label for="snome">Complemento</label>
+                  <input type="text" id="snome" name="snome" style="width: 10em" value="">
+              </div>
+             </fieldset>
+             <fieldset class="grupo">
+               <div class="campo">
+                 <div id="estado">
+                   <label for="estado">Estado</label>
+            		    <select name="estado"> <!-- Só marca uma unica opção, igual o radio -->
+            			  <option value="0" selected="true">Selecione: </option>            <!-- AJAX= se tivesse um campo cidade, ele so apareceria se uma cidade fosse escolhida.-->
+            			  <option value="1">Minas Gerais </option>
+            			  <option value="2">Rio de Janeiro</option>
+            			  <option value="3">São Paulo </option>
+            			</select>
+            		</div>
+             </div>
+
+            <div class="campo">
+              <div id="cidades">
+                <label for="cidades">Cidade</label>
+         		    <select name="cidade"> <!-- Só marca uma unica opção, igual o radio -->
+         			  <option value="0" selected="true">Selecione: </option>            <!-- AJAX= se tivesse um campo cidade, ele so apareceria se uma cidade fosse escolhida.-->
+         			  <option value="1">João Monlevade </option>
+         			  <option value="2">Itabira </option>
+         			  <option value="3">Belo Horizonte </option>
+         			  <option value="4">Itaúna </option>  <!-- O selected serve pra qando usarmos o limpar,dai ele volta como padrao-->
+         			  <option value="5">Alvinópolis </option>
+         			</select>
+         		</div>
+          </div>
+        </fieldset>
+          <div class="campo">
+              <label>Sexo</label>
+              <label>
+                  <input type="radio" name="sexo" value="masculino"> Masculino
+              </label>
+              <label>
+                  <input type="radio" name="sexo" value="feminino"> Feminino
+              </label>
+          </div>
+          <fieldset class="grupo">
+          <div class="campo">
+              <label for="rg">RG</label>
+              <input type="text" id="rg" name="rg" style="width: 10em" value="">
+          </div>
+          <div class="campo">
+              <label for="cpf">CPF</label>
+              <input type="text" id="cpf" name="cpf" style="width: 10em" value="">
+          </div>
+        </fieldset>
+
+
+      <fieldset class="grupo">
+        <div class="campo">
+            <label for="telefone">Telefone</label>
+            <input type="tel" id="telefone" name="telefone" style="width: 10em" value="">
+        </div>
+        <div class="campo">
+            <label for="celular">Celular</label>
+            <input type="tel" id="celular" name="celular" style="width: 10em" value="">
+        </div>
+      </fieldset>
+        <div class="campo">
+            <label for="email">E-mail</label>
+            <input type="text" id="email" name="email" style="width: 20em" value="">
+        </div>
+
+      <fieldset class="grupo">
+        <div class="campo">
+         <label for="senha">Senha</label>
+   		   <input type="password" name="senha"><br>
+       <label for="confirmacao">Confirmacao</label>
+      <input type="password" name="confirmacao"><br>
+       </div>
+     </fieldset>
+
+
+      <fieldset class="grupo">
+         <div class="campo">
+           <input type="button" onClick="window.open('sucessoCadastro.html')" value="Enviar">
+        </div>
+           <div class="campo">
+           <input type="reset" value= "Limpar">
+         </div>
+
+      </fieldset>
+
+      </fieldset>
+  </form>
+</div>
+</div>
+
+<div class="starter">
+<footer class="bd-footer text-muted">
+ <div class="container">
+   <center>
+   <h4>Social Media</h4>
+   <a href="https://www.facebook.com/devaneios.abstratos/?ref=br_rs"> <img src="icons/facebook.png" width=50px;/> </a>
+   <a href="https://www.instagram.com/dasaxavierart/"> <img src="icons/instagram.png" width=70px;/></a>
+ </center>
+   <p> Desenvolvido por Dasayeve Xavier & Deise Silva para a displicina WEBI</p>
+</div>
+</footer>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+
+
+  </body>
+
+
+
+  </html>
