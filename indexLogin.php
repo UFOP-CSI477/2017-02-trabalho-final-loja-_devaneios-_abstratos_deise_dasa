0b2e302b99203fb33643d566f9bc2c4dd9263dde
Dasayeve <dks.xavier@gmail.com>
2018-02-13 02:00:38 -0200

    Login, Logoff
    Atualizado

--- indexLogin.php
+++ indexLogin.php
@@ -39,27 +39,21 @@
             <li><a href="sobre.php">Sobre</a></li>
             <li><a href="tirinhas.php">Tirinhas</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>OLá</b> <span class="caret"></span></a>
       <ul id="login-dp" class="dropdown-menu">
         <li>
            <div class="row">
               <div class="col-md-12">
-                 <form class="form" role="form" method="post" action="login.php" accept-charset="UTF-8" id="login-nav">
+                 <form class="form" role="form" method="post" action="logoff.php" accept-charset="UTF-8" id="login-nav">
+                   <div class="bottom text-center">
+                    <?php echo "Olá $usuario" . "</br>" ?>
+                    Gerenciar <a href="#"><b>minha conta</b></a>
+                   </div>
+
                     <div class="form-group">
-                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                       <input type="email" class="form-control" id="exampleInputEmail2" name="email" placeholder="Email" required>
-                    </div>
-                    <div class="form-group">
-                       <label class="sr-only" for="exampleInputPassword2">Password</label>
-                       <input type="password" class="form-control" id="exampleInputPassword2" name="senha" placeholder="Senha" required>
-                    </div>
-                    <div class="form-group">
-                         <button type="submit" class="btn btn-primary btn-block">Logar</button>
+                         <button type="submit" class="btn btn-primary btn-block">Logoff</button>
                       </div>
                    </form>
-                </div>
-                <div class="bottom text-center">
-                 Não tem cadastro? <a href="cadastro.php"><b>Cadastre-se</b></a>
                 </div>
              </div>
           </li>
--- login.php
+++ login.php
@@ -5,31 +5,38 @@ session_start();
 $login = $_POST["email"];
 $senha = $_POST["senha"];
 
+try{
 $db = new PDO('mysql:host=localhost;dbname=devaneiosLoja','devaneiosloja', '123456');
+}catch(PDOException $e){
+  echo 'Connection failed: ' . $e->getMessage();
 
+}
 
 
 // A variavel $result pega as varias $login e $senha, faz uma pesquisa na tabela de usuarios
-//$db ->query("UPDATE cliente SET nome = 'Lucas Ramos' WHERE id= 3;");
-$result = $db->query("SELECT * FROM cliente WHERE email = '$login' AND SENHA = '$senha'");
+
+$result = $db->query("SELECT * FROM `cliente` WHERE `email` = '$login' AND `SENHA` = '$senha'");
 
 //checa result se maior que 0 quer dizer que deu certo
-if(mysql_num_rows ($result) > 0 ) {
-  $_SESSION['login'] = $login;
-  $_SESSION['senha'] = $senha;
-  $_SESSION['nome'] = $result['nome'];
-  $_SESSION['logado']=1;
-  echo "sucesso";
-//header('location:index.php');
+$row_cnt = count($result);
+$linha = $result->fetch(PDO::FETCH_ASSOC);
+if($row_cnt > 0 ) {
+  $_SESSION["login"] = $login;
+  $_SESSION["senha"] = $senha;
+  $_SESSION["nome"] = $linha["nome"];
+  $_SESSION["logado"]=1;
+//  echo $_SESSION['nome'];
+//  echo "sucesso";
+header('location:indexLogin.php');
 } else {
     unset ($_SESSION['login']);
     unset ($_SESSION['senha']);
     unset ($_SESSION['nome']);
     unset ($_SESSION['logado']);
-    print_r($db);
+    //print_r($db);
     print_r($result);
     //print_r($result);
-    //header('location:index.php');
+    header('location:index.php');
 
     }
 
